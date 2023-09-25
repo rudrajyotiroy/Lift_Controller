00e243252301dee4869fc3e6fe4730aa56b516c9
rudrajyotiroy <rudrajyotiroy@gmail.com>
2023-09-26 00:42:59 +0530

    UVM Env Setup Commit 1

---
+++ verif/env/agents/lift_controller_cfg.sv
@@ -0,0 +1,20 @@
+`ifndef LIFT_CONTROLLER_CFG
+`define LIFT_CONTROLLER_CFG
+
+typedef enum { UP, DN, STOP, NULL } lift_request;
+typedef enum { MODERATE, HEAVY, LIGHT, SCARCE, CONVERGING, DIVERGING, PAIRED, TWO_WAY_CONV, TWO_WAY_DIV, PAIRED_W_HOTSPOT, CUSTOM} op_cond;
+
+class lift_controller_cfg #(parameter N_FLOORS = 12) extends uvm_sequence_item;
+    rand lift_request req;
+    rand op_cond traffic;
+    rand int floor;
+    rand int delay;
+
+    // For modelling specialized testcases
+    rand int pref_floor_1;
+    rand int pref_floor_2;
+    rand int pref_floor_3;
+
+endclass
+
+`endif
---
+++ verif/tb/src/lift_movement_emulator.sv
@@ -0,0 +1,60 @@
+`ifndef LIFT_MOVEMENT_EMULATOR
+`define LIFT_MOVEMENT_EMULATOR
+// Specify time period in clks per floor
+// Direction is 1 up 0 dn
+// True floor state is <gnd>.....<top>
+module lift_movement_emulator #(
+    parameter N_FLOORS = 12,
+    parameter T = 200,
+    parameter T_FLR_CONTCT = 50
+) (
+    input clk,
+    input direction,
+    input motion,
+    output logic [N_FLOORS-1:0] floor_sense
+);
+
+// Goal : Realistic model, consider all scenarios
+reg [N_FLOORS-1:0] true_floor_state;
+integer count;
+
+initial begin
+    true_floor_state = 12'd1;
+    floor_sense = true_floor_state;
+    count = 0;
+end
+
+always @(posedge clk) begin
+    if(motion) begin
+        count = direction ? count + 1 : count - 1;
+        if(count == T) begin
+            true_floor_state = true_floor_state << 1;
+            floor_sense = true_floor_state;
+            count = 0;
+        end
+        if(count == -T) begin
+            true_floor_state = true_floor_state >> 1;
+            floor_sense = true_floor_state;
+            count = 0;
+        end
+        if(count > T_FLR_CONTCT) begin
+            floor_sense = {N_FLOORS{1'b0}};
+        end
+        if(count < -T_FLR_CONTCT) begin
+            floor_sense = {N_FLOORS{1'b0}};
+        end
+    end
+end
+
+// Space for movement constraint assertions
+
+// Assertion 0 : Lift direction should not change when lift is in motion
+// Assertion 1 : Lift should not stop in between floors except for reset
+// Assertion 2 : Lift should never continue movement beyond ground floor or top floor
+// Assertion 3 : Door can only open when lift is stationary
+// Assertion 4 : True floor state should be one hot always (indicating last floor left) 
+// Assertion 5 : Once door open, should stay open for DOOR_OPEN_REQ clock cycles
+
+endmodule
+
+`endif
